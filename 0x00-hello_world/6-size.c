d830e9e68512c499cf5c8489c87bb745bfe922cc
Francis Onwumbiko <francis2onwumbiko@gmail.com>
2022-09-11 12:16:48 -0700

    six commit at once

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main -print the string in the put function
+ *
+ * Description: using the main function
+ * this program prints "Programming is like building a multilingual puzzle"
+ * Return: 0
+ */
+
+int main(void)
+
+{
+
+		printf("Size of a char: %zu byte(s)\n", sizeof(char));
+
+			printf("Size of an int: %zu byte(s)\n", sizeof(int));
+
+				printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+
+					printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+
+						printf("Size of a float: %zu byte(s)\n", sizeof(float));
+
+							return (0);
+
+}
