491c894dae93982fa4b716c0e11ca6c1819dee9d
Francis Onwumbiko <francis2onwumbiko@gmail.com>
2022-09-14 18:21:30 -0700

    5. Sign

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ *
+ * Return: 0
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char r, d;
+	for (d = 0 ; d <= 9 ; d++)
+	{
+		 for (r = 'a' ; r <= 'z' ; r++)
+		 {
+			 _putchar(r);
+		 }
+		  _putchar('\n');
+	}
+}
+
---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - prints a sign based on n
+ * @n: integer to input
+ * Return: 1 or 0  or -1
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
+
