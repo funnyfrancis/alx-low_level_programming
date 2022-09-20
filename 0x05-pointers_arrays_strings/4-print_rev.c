26f7d95118967556d87deb24969ede69fe14f763
Francis Onwumbiko <francis2onwumbiko@gmail.com>
2022-09-20 13:43:38 -0700

    nice

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,17 +2,17 @@
 #include <string.h>
 #include "main.h"
 
- /**
- *print_rev - prints a string in reverse order
- *@s: string to reverse
- *Return: Nothing
- */
+/**
+*print_rev - prints a string in reverse order
+*@s: string to reverse
+*Return: Nothing
+*/
 
-void print_rev(char *s);
+void print_rev(char *s)
 {
 	int len = strlen (s);
 
-	while(len--)
+	while (len--)
 		putchar(*(s + len));
 	putchar(10);
 }
