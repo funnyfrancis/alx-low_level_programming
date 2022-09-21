596dcf8ce20f39a3a667cc1e02f3c1cd8b6abfb0
Francis Onwumbiko <francis2onwumbiko@gmail.com>
2022-09-21 15:15:57 -0700

    fantastic

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,10 +2,11 @@
 #define MAIN_H
 #include <stdio.h>
 /**
-*void prototypes()
-*int prototypes()
-*char prototypes()
-*/
+ *void prototypes()
+ *int prototypes()
+ *char prototypes()
+ */
+
 void reverse_array(int *a, int n);
 void print_number(int n);
 void print_buffer(char *b, int size);
@@ -15,7 +16,9 @@ char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
+char *leet(char *);
 char *rot13(char *);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+char *add_strings(char *n1, char *n2, char *r, int r_index);
 
-#endif/*MAIN_H*/
+#endif /*MAIN_H*/
