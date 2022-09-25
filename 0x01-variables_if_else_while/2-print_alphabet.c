4cd49059af9d8716c81955c623299b4fee2088a2
Francis Onwumbiko <francis2onwumbiko@gmail.com>
2022-09-25 07:46:58 -0700

    3. alphABET

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - prints lowercase alphabets
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
+
