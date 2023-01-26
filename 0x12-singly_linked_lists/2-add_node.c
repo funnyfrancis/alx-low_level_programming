0210693e63ef7d798e93e101dcaafd5b8fb1e31d
funnyfrancis <francis2onwumbiko@gmail.com>
2023-01-26 13:56:52 -0800

    Real programmers can write assembly code in any language

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,28 +14,28 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	char *dup;
-        int len;
-        list_t *new;
+	int len;
+	list_t *new;
 
 	new = malloc(sizeof(list_t));
-        if (new == NULL)
+	if (new == NULL)
 		return (NULL);
 	dup = strdup(str);
-        if (dup == NULL)
+	if (dup == NULL)
 	{
 		free(new);
-                return (NULL);
+		return (NULL);
 	}
 
 	for (len = 0; str[len];)
-                len++;
+		len++;
 
 	 new->str = dup;
-         new->len = len;
-         new->next = *head;
-
+	 new->len = len;
+	 new->next = *head;
+	 
 	 *head = new;
-
-	  return (new);
+	 
+	 return (new);
 }
 
